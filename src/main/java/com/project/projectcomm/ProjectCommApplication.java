78d9388769d49a42a34ca31383c4539551665fe0
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-03-02 17:02:00 +0900

    first commit

---
+++ src/main/java/com/project/projectcomm/ProjectCommApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCommApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/projectcomm/ProjectCommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
